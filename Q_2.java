cb6ca49d9aac591b903799d1d4c1292e98bc23de
mehmet surucu <mehmetsmarket@gmail.com>
2021-12-04 20:46:40 +0300

    04Dec

---
+++ Q_2.java
@@ -0,0 +1,26 @@
+package OCA_Questions;
+
+public class Q_2 {
+
+	public static void main(String[] args) {
+		
+		// What's the result?
+		
+		String strs []= {"A", "B"};
+		
+		int idx=0;
+		
+		for (String s : strs) {
+			strs[idx].concat("element"+idx);
+			
+			idx++;
+		}
+
+		for (idx = 0; idx < strs.length; idx++) {
+		
+			System.out.print(strs[idx]);
+		}
+		
+	}
+
+}
