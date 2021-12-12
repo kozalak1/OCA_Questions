59bc77d6d79218745263f42ccbb92225b12bf064
mehmet surucu <mehmetsmarket@gmail.com>
2021-12-12 23:25:08 +0300

    12Dec

---
+++ AsalSayi.java
@@ -0,0 +1,19 @@
+package OCA_Questions;
+
+public class AsalSayi {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		Ask user enter a positive number and check if it is prime or not
+
+		Kullanıcıdan pozitif bir sayı girmesini isteyin ve asal olup olmadığını kontrol edin
+		 */
+		
+		
+		
+
+	}
+
+}
