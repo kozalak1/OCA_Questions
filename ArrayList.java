a5b15e301e75f851b369e4cf3f9d84d1c6a8cac5
mehmet surucu <mehmetsmarket@gmail.com>
2021-12-20 23:43:54 +0300

    20dec

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+package OCA_Questions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		
+		String [] arr = {"Hi", "How", "Are", "You"};
+		List<String> arrList = new ArrayList<> (Arrays.asList(arr)) ;
+		if (arrList.removeIf(null) {
+			
+			
+		}
+				
+
+	}
+
+}
