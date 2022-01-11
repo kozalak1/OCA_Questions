207fce6fdf94326ca9e96e9062320071978d215d
mehmet surucu <mehmetsmarket@gmail.com>
2022-01-11 19:40:35 +0300

    11Jan

---
+++ Concatenation.java
@@ -0,0 +1,15 @@
+package OCA_Questions;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Result A " + 0 + 1);
+		System.out.println("Result B " + (1) + (2));
+	}
+		// What is the result?
+	
+		//  Result A 01
+		//	Result B 12
+}
+// ilk değer String bir ifade oldugundan sonraki rakamlara da matemetiksel toplama işlemi yapmadı, concat yaptı ve String bir sonuc döndürdü
